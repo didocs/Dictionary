c0264ab841538b36b16d5625267674b891779eaf
Daniel Bercea <128615941+didocs@users.noreply.github.com>
2023-08-21 14:55:59 +0300

    Update scriptDictionary.js

--- scriptDictionary.js
+++ scriptDictionary.js
@@ -1,23 +1,22 @@
-let wordList = [];
+let wordList = [], target, value;
+
+function displayMessage(target,value) {
+    target.innerText = value;
+    setTimeout( () => {target.innerText = ""; }, 2000);
+}
 
 function addWord() {
     let word = document.getElementById("wordInput");
     let textMessage = document.getElementById("message-input");
     wordList.splice(0, 0, word.value);
-    textMessage.innerText =  word.value + " successfully added!";
+    displayMessage(textMessage, word.value + " successfully added!");
     word.value = "";
-   	setTimeout(() => {textMessage.innerText = ""; }, 2000);
 }
-
 
 function searchWord() {
     let entry = document.getElementById("search");
     let textOutput = document.getElementById("message-search");
     let find = wordList.indexOf(entry.value); 
-    find = find === -1 ?  textOutput.innerText = "No match found!" :  textOutput.innerHTML = "Dictionary contains the word " + entry.value + "!";
+    find = find === -1 ?  displayMessage(textOutput,"No match found!") : displayMessage(textOutput,"Dictionary contains the word " + entry.value + "!");
     entry.value = "";
-   	setTimeout( () => {textOutput.innerText = ""; }, 2000);
 }
-
-
-
