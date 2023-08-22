b1272d1231a2dbb801bf20ca8fabc757f09f4c05
Daniel Bercea <128615941+didocs@users.noreply.github.com>
2023-08-22 11:45:30 +0300

    Update scriptDictionary.js

--- scriptDictionary.js
+++ scriptDictionary.js
@@ -17,6 +17,6 @@ function searchWord() {
     let entry = document.getElementById("search");
     let textOutput = document.getElementById("message-search");
     let find = wordList.indexOf(entry.value); 
-    find = find === -1 ?  displayMessage(textOutput,"No match found!") : displayMessage(textOutput,"Dictionary contains the word " + entry.value + "!");
+    find === -1 ?  displayMessage(textOutput,"No match found!") : displayMessage(textOutput,"Dictionary contains the word " + entry.value + "!");
     entry.value = "";
 }
